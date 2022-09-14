bc9b103c8742d05d9bac613cf3ca0ae31f95fff4
Olajide <oolajide91@gmail.com>
2022-09-14 05:08:00 +0100

    Maintenance Page Feature

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     failOnStatusCode: false,
     experimentalSessionAndOrigin: true,
+    retries: 1,
+    pageLoadTimeout:60000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/Orange HR Management/HRM.cy.js
+++ cypress/e2e/Orange HR Management/HRM.cy.js
@@ -172,6 +172,61 @@ describe ('Orange HR Management Test Suite', () => {
 
     })
 
+
+    it('Navigate to Maintenance Page', () => {
+
+        cy.get('.orangehrm-login-slot-wrapper')
+        cy.get('.oxd-text--h5').should('have.text', 'Login')
+
+        //Login to ORM as an admin user
+        cy.get('.orangehrm-login-slot')
+        cy.get('.orangehrm-login-form')
+
+        //admin enters correct username
+        cy.get(':nth-child(2) > .oxd-input-group')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
+
+        //admin enters correct password
+        cy.get(':nth-child(3) > .oxd-input-group')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password, {log:false})
+
+        //admin clicks on login button
+        cy.get('.oxd-button').click();
+
+        cy.get('.oxd-sidepanel-body')
+        cy.get('ul.oxd-main-menu')
+        cy.get(':nth-child(10) > .oxd-main-menu-item')
+        cy.contains('Maintenance').click();
+
+        //Admin types password
+        cy.get('#app')
+        cy.get('.orangehrm-admin-access-container')
+        cy.get('.orangehrm-card-container')
+        cy.get(':nth-child(6) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password, {log:false})
+        cy.get('.oxd-button--secondary').click({force:true})
+
+        // //verify that we have landed the Maintenance page
+         cy.url().should('include', 'https://opensource-demo.orangehrmlive.com/web/index.php/maintenance/purgeEmployee')
+        cy.get('body')
+        cy.get('.oxd-layout-container')
+        cy.get('.oxd-layout-context')
+        cy.get('.orangehrm-background-container')
+        cy.get('.oxd-topbar-body-nav > ul > :nth-child(2)').click();
+
+        // //search for an employee
+        cy.get('.oxd-form')
+        cy.get('.oxd-autocomplete-text-input > input').type('Odis')
+            // .trigger("mousemove", 50, 50, {force:true})
+            // .trigger("mousedown", {which : 1})
+            // .trigger("mouseup", {which : 1})
+        //cy.wait(5000)
+        //cy.find('-oxd-autocomplete-dropdown --position-bottom').contains('Odis Adalwin')
+            .click({force:true})
+        return this;
+
+    })
+
+
     after( () => {
         //Logout User
         cy.get('.oxd-topbar-header-userarea')
